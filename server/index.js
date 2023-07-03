7813c9274ce30c85c2bdf3c0a10ad1727faaf646
Abhishek-kumar-verma <abhishek05kv@gmail.com>
2023-07-03 08:26:55 +0530

    till sidebar

---
+++ server/index.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan';
+import dotenv from "dotenv";
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
+
+// data insert in mongodb
+import User from "./models/User.js";
+import Product from './models/Product.js';
+import ProductStat from './models/ProductStat.js';
+import {dataUser , dataProduct , dataProductStat} from "./data/index.js";
+
+// configurations
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy : "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cors());
+
+// mongoose connection
+const PORT = process.env.PORT || 9000 ;
+mongoose.connect(process.env.MONGO_URL , {
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+}).then(()=>{
+    app.listen(PORT , ()=> console.log(`Server running on port : ${PORT}`));
+
+    // data insert only once
+    // User.insertMany(dataUser);
+    // Product.insertMany(dataProduct);
+    // ProductStat.insertMany(dataProductStat);
+    
+}).catch((error)=> console.log(`${error} did not run`));
+
+
+
+// routes
+app.use("/client" , clientRoutes);
+app.use("/sales", salesRoutes);
+app.use("/general", generalRoutes);
+app.use("/management" , managementRoutes);
